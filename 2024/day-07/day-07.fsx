39018edca4746de1ab082486371a966bc7e5abe9
Jeffrey Bissel <jeffrey@bissel.it>
2024-12-07 12:38:22 +0100

    Solved day 7 part 2

--- 2024/day-07/day-07.fsx
+++ 2024/day-07/day-07.fsx
@@ -10,7 +10,7 @@ let data = Utilities.getData "./day-07/input.txt" " "
 
 let debugRow (s: uint64, num: uint64 seq) = $"{s}: {num |> Seq.map string |> Utilities.debugConcat }"
 
-let checkEquation (operators: _ list) (result: uint64, num: uint64 seq) =
+let checkEquation (operators: (uint64 -> uint64 -> uint64) list) (result: uint64, num: uint64 seq) =
     let rec _eval (num: uint64 list) =
         match num with
             | [] -> [uint64 0] |> List.toSeq
@@ -18,10 +18,9 @@ let checkEquation (operators: _ list) (result: uint64, num: uint64 seq) =
             | b :: tail ->
                 let pre = _eval tail
                 seq {
-                    // for op in operators do
+                    for op in operators do
                         for eq in pre do
-                            yield (+) eq b
-                            yield (*) eq b
+                            yield op eq b
                 }
             
     let possibleEvaluations = num |> Seq.rev |> Seq.toList |> _eval
@@ -29,7 +28,7 @@ let checkEquation (operators: _ list) (result: uint64, num: uint64 seq) =
     possibleEvaluations |> Seq.exists ((=) result)
 
 let equations = data
-                   |> Seq.where (checkEquation [(+), (*)])
+                   |> Seq.where (checkEquation [(+); (*)])
 
 let result1 = equations |> Seq.map fst |> Seq.sum
 
@@ -37,7 +36,11 @@ printfn $"Result 1: {result1}"
 
 // -------------------------------------------------- //
 
-let result2 = "-"
+let concat (a: uint64) (b: uint64) = uint64 $"{a}{b}" 
+
+let equations2 = data
+                   |> Seq.where (checkEquation [(+); (*); concat])
+let result2 = equations2 |> Seq.map fst |> Seq.sum
 printfn $"Result 2: {result2}"
 
 exit 0
