da391e40967959f2d469005d477865aa345dabf1
Jeffrey Bissel <jeffrey@bissel.it>
2024-12-12 17:10:28 +0100

    Solved Day 12 part 2

--- 2024/day-12/day-12.fsx
+++ 2024/day-12/day-12.fsx
@@ -8,16 +8,14 @@ let data =
    
 let width, height, _ = data |> List.last
 
-printfn $"{width} {height}"
 let matrix = Array2D.init
                  (width + 1)
                  (height + 1)
                  (fun X Y -> data |> List.find (fun (x,y,_) -> x = X && y = Y ) |> fun (_,_,c) -> c )
 
-// has boundaries: Top, Right, Bottom, Left
-type Boundarie = (int*int)*(int*int)
+type Boundary = (int*int)*(int*int)
 
-let getBoundaries (data: char array2d): Boundarie seq =
+let getBoundaries (data: char array2d): Boundary seq =
     let bounds =
         Array2D.init
             (width + 1)
@@ -96,7 +94,7 @@ let getRegions (data: char array2d): Region list =
     
     areas 
 
-let getPerimeter (bounds: Boundarie seq) (region: Region): int =
+let getPerimeter (bounds: Boundary seq) (region: Region): int =
     snd region
     |> Seq.map (fun pos -> bounds |> Seq.where (fun (a,_) -> a = pos) |> Seq.length)
     |> Seq.sum
@@ -112,7 +110,62 @@ printfn $"Result 1: {result1}"
 
 // -------------------------------------------------- //
 
-let result2 = "-"
+let getSides (bounds: Boundary seq) (region: Region): int =
+    let boundaries =
+        snd region
+        |> Seq.map (fun pos -> bounds |> Seq.where (fun (a,_) -> a = pos) )
+        |> Seq.concat
+        |> Seq.toList
+        
+    let edges =
+        boundaries
+            |> Seq.groupBy (fun ((aX, aY), (bX, bY)) ->
+                if aX = bX then
+                    if aY > bY then 't' else 'b'
+                else
+                    if aX > bX then 'l' else 'r'
+            )
+            |> Seq.toList
+            
+    let edgePositions (t: char) (by: int*int -> int) (sel: int*int -> int) =
+        edges
+        |> Seq.where (fun e -> ((=) t) (fst e))
+        |> Seq.head
+        |> snd
+        |> Seq.map fst
+        |> Seq.groupBy by
+        |> Seq.sortBy fst
+        |> Seq.map snd
+        |> Seq.map (Seq.map sel)
+        |> Seq.map Seq.sort
+        |> Seq.map (fun p ->
+            (1, p |> Seq.pairwise)
+             ||> Seq.fold (fun res (a,b) -> if (b - a) > 1 then res + 1 else res)
+        )
+        |> Seq.sum
+            
+    let t = edgePositions 't' snd fst
+    let b = edgePositions 'b' snd fst
+    
+    let l = edgePositions 'r' fst snd
+    let r = edgePositions 'l' fst snd
+    
+    t + b + l + r
+
+let regionSides = regions |> Seq.map (getSides bounds) |> Seq.toList
+let regionWithSides = Seq.zip (regions |> Seq.map snd |> Seq.map Seq.length ) regionSides
+
+// let s = regions
+//         |> Seq.mapi (fun i (c, region) ->
+//             let s = regionSides[i]
+//             let r = region.Length
+//             printfn $"{c}: {r} {s} = {r * s}"
+//             r * s
+//         ) |> Seq.toList
+//         
+// printfn $"{s |> Seq.sum}"
+
+let result2 = regionWithSides |> Seq.map (fun (a,b) -> a * b ) |> Seq.sum
 printfn $"Result 2: {result2}"
 
 exit 0
