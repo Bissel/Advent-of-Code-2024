a8284cb88f22bf922e42a27ead1b048d024b6174
Jeffrey Bissel <jeffrey@bissel.it>
2024-12-13 10:57:39 +0100

    Added day 13

---
+++ 2024/day-13/day-13.fsx
@@ -0,0 +1,16 @@
+#load @"../AdventOfCode/Utilities.fs"
+
+open AdventOfCode
+
+let data = Utilities.readLines "./day-13/example.txt"
+
+let result1 = "-"
+
+printfn $"Result 1: {result1}"
+
+// -------------------------------------------------- //
+
+let result2 = "-"
+printfn $"Result 2: {result2}"
+
+exit 0
